e7a6c715a18259d6fd3f1a50b1db0617042ff03f
Victor Tolbert <victor.tolbert@gmail.com>
2017-09-29 16:35:37 -0400

    wip...

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,24 +35,24 @@
                 <div class="tile is-child box">
                     <p class="title">Overview</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/overview/start">Start</a>
-                        <a class="tag is-large" href="/overview/customize">Customize</a>
-                        <a class="tag is-large" href="/overview/classes">Classes</a>
-                        <a class="tag is-large" href="/overview/modular">Modular</a>
-                        <a class="tag is-large" href="/overview/responsiveness">Responsiveness</a>
-                        <a class="tag is-large" href="/overview/variables">Variables</a>
-                        <a class="tag is-large" href="/overview/colors">Colors</a>
-                        <a class="tag is-large" href="/overview/functions">Functions</a>
-                        <a class="tag is-large" href="/overview/mixins">Mixins</a>
+                        <a class="tag is-medium" href="/overview/start">Start</a>
+                        <a class="tag is-medium" href="/overview/customize">Customize</a>
+                        <a class="tag is-medium" href="/overview/classes">Classes</a>
+                        <a class="tag is-medium" href="/overview/modular">Modular</a>
+                        <a class="tag is-medium" href="/overview/responsiveness">Responsiveness</a>
+                        <a class="tag is-medium" href="/overview/variables">Variables</a>
+                        <a class="tag is-medium" href="/overview/colors">Colors</a>
+                        <a class="tag is-medium" href="/overview/functions">Functions</a>
+                        <a class="tag is-medium" href="/overview/mixins">Mixins</a>
                     </p>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Modifiers</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/modifiers/syntax">Syntax</a>
-                        <a class="tag is-large" href="/modifiers/helpers">Helpers</a>
-                        <a class="tag is-large" href="/modifiers/responsive-helpers">Responsive Helpers</a>
-                        <a class="tag is-large" href="/modifiers/typography-helpers">Typography Helpers</a>
+                        <a class="tag is-medium" href="/modifiers/syntax">Syntax</a>
+                        <a class="tag is-medium" href="/modifiers/helpers">Helpers</a>
+                        <a class="tag is-medium" href="/modifiers/responsive-helpers">Responsive Helpers</a>
+                        <a class="tag is-medium" href="/modifiers/typography-helpers">Typography Helpers</a>
                     </p>
                     <div class="paging">
                         <a>M</a>
@@ -70,35 +70,35 @@
                 <div class="tile is-child box">
                     <p class="title">Columns</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/columns/basics">Basics</a>
-                        <a class="tag is-large" href="/columns/sizes">Sizes</a>
-                        <a class="tag is-large" href="/columns/responsiveness">Responsiveness</a>
-                        <a class="tag is-large" href="/columns/nesting">Nesting</a>
-                        <a class="tag is-large" href="/columns/gap">Gap</a>
-                        <a class="tag is-large" href="/columns/options">Options</a>
+                        <a class="tag is-medium" href="/columns/basics">Basics</a>
+                        <a class="tag is-medium" href="/columns/sizes">Sizes</a>
+                        <a class="tag is-medium" href="/columns/responsiveness">Responsiveness</a>
+                        <a class="tag is-medium" href="/columns/nesting">Nesting</a>
+                        <a class="tag is-medium" href="/columns/gap">Gap</a>
+                        <a class="tag is-medium" href="/columns/options">Options</a>
                     </p>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Layout</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/layout/container">Container</a>
-                        <a class="tag is-large" href="/layout/level">Level</a>
-                        <a class="tag is-large" href="/layout/media">Media Object</a>
-                        <a class="tag is-large" href="/layout/hero">Hero</a>
-                        <a class="tag is-large" href="/layout/section">Section</a>
-                        <a class="tag is-large" href="/layout/footer">Footer</a>
-                        <a class="tag is-large" href="/layout/tiles">Tiles</a>
+                        <a class="tag is-medium" href="/layout/container">Container</a>
+                        <a class="tag is-medium" href="/layout/level">Level</a>
+                        <a class="tag is-medium" href="/layout/media">Media Object</a>
+                        <a class="tag is-medium" href="/layout/hero">Hero</a>
+                        <a class="tag is-medium" href="/layout/section">Section</a>
+                        <a class="tag is-medium" href="/layout/footer">Footer</a>
+                        <a class="tag is-medium" href="/layout/tiles">Tiles</a>
                     </p>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Form Controls</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/form/input">Input</a>
-                        <a class="tag is-large" href="/form/textarea">Textarea</a>
-                        <a class="tag is-large" href="/form/select">Select</a>
-                        <a class="tag is-large" href="/form/checkbox">Checkbox</a>
-                        <a class="tag is-large" href="/form/radio">Radio</a>
-                        <a class="tag is-large" href="/form/file">File</a>
+                        <a class="tag is-medium" href="/form/input">Input</a>
+                        <a class="tag is-medium" href="/form/textarea">Textarea</a>
+                        <a class="tag is-medium" href="/form/select">Select</a>
+                        <a class="tag is-medium" href="/form/checkbox">Checkbox</a>
+                        <a class="tag is-medium" href="/form/radio">Radio</a>
+                        <a class="tag is-medium" href="/form/file">File</a>
                     </p>
                 </div>
             </div>
@@ -106,34 +106,35 @@
                 <div class="tile is-child box">
                     <p class="title">Elements</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/elements/box">Box</a>
-                        <a class="tag is-large" href="/elements/button">Button</a>
-                        <a class="tag is-large" href="/elements/content">Content</a>
-                        <a class="tag is-large" href="/elements/delete">Delete</a>
-                        <a class="tag is-large" href="/elements/icon">Icon</a>
-                        <a class="tag is-large" href="/elements/image">Image</a>
-                        <a class="tag is-large" href="/elements/notification">Notification</a>
-                        <a class="tag is-large" href="/elements/progress">Progress</a>
-                        <a class="tag is-large" href="/elements/table">Table</a>
-                        <a class="tag is-large" href="/elements/tag">Tag</a>
-                        <a class="tag is-large" href="/elements/title">Title</a>
+                        <a class="tag is-medium" href="/elements/box">Box</a>
+                        <a class="tag is-medium" href="/elements/button">Button</a>
+                        <a class="tag is-medium" href="/elements/content">Content</a>
+                        <a class="tag is-medium" href="/elements/delete">Delete</a>
+                        <a class="tag is-medium" href="/elements/icon">Icon</a>
+                        <a class="tag is-medium" href="/elements/image">Image</a>
+                        <a class="tag is-medium" href="/elements/notification">Notification</a>
+                        <a class="tag is-medium" href="/elements/progress">Progress</a>
+                        <a class="tag is-medium" href="/elements/table">Table</a>
+                        <a class="tag is-medium" href="/elements/tag">Tag</a>
+                        <a class="tag is-medium" href="/elements/title">Title</a>
                     </p>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Components</p>
                     <p class="tags">
-                        <a class="tag is-large" href="/components/datepicker">Datepicker</a>
-                        <a class="tag is-large" href="/components/popover">Popover</a>
-                        <a class="tag is-large" href="/components/breadcrumb">Breadcrumb</a>
-                        <a class="tag is-large" href="/components/card">Card</a>
-                        <a class="tag is-large" href="/components/dropdown">Dropdown</a>
-                        <a class="tag is-large" href="/components/menu">Menu</a>
-                        <a class="tag is-large" href="/components/message">Message</a>
-                        <a class="tag is-large" href="/components/modal">Modal</a>
-                        <a class="tag is-large" href="/components/navbar">Navbar</a>
-                        <a class="tag is-large" href="/components/pagination">Pagination</a>
-                        <a class="tag is-large" href="/components/panel">Panel</a>
-                        <a class="tag is-large" href="/components/tabs">Tabs</a>
+                        <a class="tag is-medium" href="/components/datepicker">Datepicker</a>
+                        <a class="tag is-medium" href="/components/popover">Popover</a>
+                        <a class="tag is-medium" href="/components/breadcrumb">Breadcrumb</a>
+                        <a class="tag is-medium" href="/components/card">Card</a>
+                        <a class="tag is-medium" href="/components/dropdown">Dropdown</a>
+                        <a class="tag is-medium is-dark" href="/data-table">Data Table</a>
+                        <a class="tag is-medium" href="/components/menu">Menu</a>
+                        <a class="tag is-medium" href="/components/message">Message</a>
+                        <a class="tag is-medium" href="/components/modal">Modal</a>
+                        <a class="tag is-medium" href="/components/navbar">Navbar</a>
+                        <a class="tag is-medium" href="/components/pagination">Pagination</a>
+                        <a class="tag is-medium" href="/components/panel">Panel</a>
+                        <a class="tag is-medium" href="/components/tabs">Tabs</a>
                   </p>
                 </div>
             </div>
--- resources/views/partials/navbars/utilities.blade.php
+++ resources/views/partials/navbars/utilities.blade.php
@@ -1,7 +1,19 @@
-<nav class="navbar is-light" role="navigation" aria-label="navigation">
-    <div class="container is-fluid">
+<nav class="navbar is-tertiary" role="navigation" aria-label="navigation">
+    {{-- <div class="container is-fluid"> --}}
         <div class="navbar-menu" id="navbar-utilites">
             <div class="navbar-brand">
+                <a class="navbar-item">
+                    <div class="tags has-addons">
+                        <span class="tag is-medium">Conflicts</span>
+                        <span class="tag is-medium is-danger">8</span>
+                    </div>
+                </a>
+                <a class="navbar-item">
+                    <div class="tags has-addons">
+                        <span class="tag is-medium">Gaps</span>
+                        <span class="tag is-medium is-danger">4</span>
+                    </div>
+                </a>
 {{--                 <span class="navbar-burger is-block" data-show="quickview" data-target="page-info">
                     <span></span>
                     <span></span>
@@ -11,6 +23,7 @@
             <div class="navbar-start">
                 {{-- @include('partials.navbar-items.search') --}}
                 {{-- @include('partials.navbar-items.filter-list') --}}
+
             </div>
             <div class="navbar-end">
                 {{-- @include('partials.navbar-items.format-size') --}}
@@ -24,5 +37,5 @@
                 {{-- @include('partials.navbar-items.more') --}}
             </div>
         </div>
-    </div>
+    {{-- </div> --}}
 </nav>
